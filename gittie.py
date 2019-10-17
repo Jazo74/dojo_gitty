cd6bfbc75af17a3393bc3624bd4020cf938431b2
Jazo74 <55430157+Jazo74@users.noreply.github.com>
2019-10-17 10:29:14 +0200

    ok

--- gittie.py
+++ gittie.py
@@ -1,10 +1,6 @@
 def introduce():
-<<<<<<< HEAD
     print("Hello, I'm Kittie!")
 
-=======
-    print("Hello, I'm not Gittie!")
->>>>>>> 2875f78527c9f7b025e6282591bf43c0d0309d62
 def joke():
     print("knock knock")
 def add(a, b):
